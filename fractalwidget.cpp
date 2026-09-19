54adc20b88b18ed32cc888deeaf0dfe337b94639
AlS0R0 <salexr2000@gmail.com>
2026-09-19 19:22:56 +0300

    Working fractal showing on the screen

--- fractalwidget.cpp
+++ fractalwidget.cpp
@@ -1,6 +1,5 @@
 #include "fractalwidget.h"
 #include "./ui_fractalwidget.h"
-// #include "complexnumber.h"
 
 FractalWidget::FractalWidget(QWidget *parent)
     : QMainWindow(parent)
@@ -12,4 +11,92 @@ FractalWidget::FractalWidget(QWidget *parent)
 FractalWidget::~FractalWidget()
 {
     delete ui;
+}
+
+QRgb getColor(const ComplexNumber& c, const double& border)
+{
+    const int maxIter = 300;
+    ComplexNumber res_num;
+    int iter = 0;
+
+    while (iter < maxIter && res_num.radius() < border) {
+        res_num = res_num.square() + c;
+        ++iter;
+    }
+
+    if (iter == maxIter) return qRgb(0, 0, 0);
+
+    double t = sqrt(double(iter) / maxIter);
+
+    int r = 0;
+    int g = static_cast<int>(255 * t);
+    int b = 0;
+
+    return qRgb(r, g, b);
+}
+
+ComplexNumber FractalWidget::pixelToComplNum(const double& px, const double& py)
+{
+    double re = xmin_ + (px / width()) * (xmax_ - xmin_);
+    double im = ymax_ - (py / height()) * (ymax_ - ymin_);
+
+    ComplexNumber res(re, im);
+    return res;
+}
+
+// void FractalWidget::renderImage()
+// {
+//     image_ = QImage(size(), QImage::Format_RGB32);
+
+//     for (int py = 0; py < height(); ++py) {
+
+//         QRgb *line = reinterpret_cast<QRgb *>(image_.scanLine(py));
+
+//         for (int px = 0; px < width(); ++px) {
+//             ComplexNumber c = pixelToComplNum(px, py);
+
+//             line[px] = getColor(c, border_);
+//         }
+//     }
+// }
+
+void FractalWidget::renderImage()
+{
+    image_ = QImage(size(), QImage::Format_RGB32);
+
+    const int SS = 2;
+    const double inv = 1.0 / (SS * SS);
+
+    for (int py = 0; py < height(); ++py) {
+        QRgb *line = reinterpret_cast<QRgb *>(image_.scanLine(py));
+
+        for (int px = 0; px < width(); ++px) {
+            int r = 0, g = 0, b = 0;
+
+            for (int sy = 0; sy < SS; ++sy) {
+                for (int sx = 0; sx < SS; ++sx) {
+                    double fx = px + (sx + 0.5) / SS;
+                    double fy = py + (sy + 0.5) / SS;
+
+                    ComplexNumber c = pixelToComplNum(fx, fy);
+                    QRgb col = getColor(c, border_);
+
+                    r += qRed(col);
+                    g += qGreen(col);
+                    b += qBlue(col);
+                }
+            }
+
+            line[px] = qRgb(int(r * inv), int(g * inv), int(b * inv));
+        }
+    }
+}
+
+void FractalWidget::paintEvent(QPaintEvent* event)
+{
+    renderImage();
+    QPainter painter(this);
+
+    painter.drawImage(0, 0, image_);
+    return;
 }
--- fractalwidget.h
+++ fractalwidget.h
@@ -2,7 +2,9 @@
 #define FRACTALWIDGET_H
 
 #include <QMainWindow>
-// #include "complexnumber.h"
+#include <QPaintEvent>
+#include <QPainter>
+#include "complexnumber.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,8 +20,21 @@ public:
     explicit FractalWidget(QWidget *parent = nullptr);
     ~FractalWidget() override;
 
+    friend QRgb getColor(ComplexNumber arg, const double& border);
+
+protected:
+    ComplexNumber pixelToComplNum(const double& px, const double& py);
+    void renderImage();
+
+    void paintEvent(QPaintEvent* event) override;
+
 private:
     Ui::FractalWidget *ui;
+    QImage image_;
+    double border_ = 4.0;
+
+    double xmin_ = -2.0, xmax_ = 0.5;
+    double ymin_ = -1.25, ymax_ = 1.25;
 };
 
 #endif // FRACTALWIDGET_H
